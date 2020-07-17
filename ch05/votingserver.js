cfa1d50f2dc38b1d6709e6373d4daae13bfd714d
Ed Torres <CoachEd@gmail.com>
2020-07-17 08:32:34 -0400

    latest

--- ch05/votingserver.js
+++ ch05/votingserver.js
@@ -26,14 +26,42 @@ app.post('/addVote',function(req, res) {
       break;
 
     default:
-      res.status(200).send('error - invalid vote: ' + vote);
+      res.status(200).send('error - invalid vote: ' + vote + '\n');
       return;
 
   }
-  res.status(200).send(voter_name + ', thank you for voting!');
+  res.status(200).send(voter_name + ', thank you for voting!\n');
   return;
 });
-  
-app.listen(5678);
+
+app.get('/getVotes', function(req, res) {
+  var str = 'Blue: ' + bluevotes + ' , ' + 'Red: ' + redvotes + '\n';
+  res.status(200).send(str);
+  return;
+});
+
+app.get('/getVote', function(req, res) {
+  var color = req.query.color;
+
+  //validate color
+  if (!color) {
+    color = "";
+  }
+  if (color !== 'blue' && color !== 'red') {
+    res.status(200).send("error - invalid color\n");  
+    return;
+  }
+    
+  var str;
+  if (color == 'blue') {
+    str = "Blue: " + bluevotes + "\n";
+  } else {
+    str = "Red: " + redvotes + "\n";
+  }
+  res.status(200).send(str);
+  return;
+});
+
+app.listen(5678); //start the server
 
 console.log('Server is running...');
