f1997d41b4de243c863b288ddaf2ed778000562b
Ed Torres <CoachEd@gmail.com>
2020-07-21 09:24:47 -0400

    latest

--- ch05/studentserver.js
+++ ch05/studentserver.js
@@ -3,7 +3,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-var fs = require('fs');
+const fs = require('fs');
+const glob = require("glob")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -52,21 +53,32 @@ app.get('/students/:record_id', function(req, res) {
 }); 
 
 app.get('/students', function(req, res) {
+  var obj = {};
+  var arr = [];
+  filesread = 0;
 
-  fs.readFile("students/" + record_id + ".json", "utf8", function(err, data) {
+  glob("students/*.json", null, function (err, files) {
+    
     if (err) {
-      var rsp_obj = {};
-      rsp_obj.record_id = record_id;
-      rsp_obj.message = 'error - resource not found';
-      return res.status(404).send(rsp_obj);
-    } else {
-      return res.status(200).send(data);
+      return res.status(500).send({"message":"error - internal server error"});
     }
-  });
 
+    for (var fname of files) {
+      fs.readFile(fname, "utf8", function(err, data) {
+        if (!err) {
+          arr.push(JSON.parse(data));
+          filesread++;
+          if (filesread == files.length) {
+            obj.students = arr;
+            return res.status(200).send(obj);
+          }
+        }
+      });
+    }
 
-  
-}); 
+  })
+
+});
 
 
 app.listen(5678); //start the server
