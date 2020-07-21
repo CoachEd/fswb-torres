a00d4e9085d8162afcd62b542401447070e2f0d4
Ed Torres <CoachEd@gmail.com>
2020-07-20 20:29:50 -0400

    latest

---
+++ ch05/buttonserver.js
@@ -0,0 +1,26 @@
+//buttonserver.js
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('./public'));
+
+app.get('/buttons', function(req, res) {
+  res.set('Content-Type', 'text/html');  //sending HTML back
+
+  var sHTML = '<!DOCTYPE html><html lang="en"><head><title>Buttons</title></head><body>';
+  for (var i=0; i < 100; i++) {
+    sHTML += '<button type="button">' + i + '</button>&nbsp;';
+  }
+  sHTML += '</body></html>';
+
+  res.status(200).send( Buffer.alloc(sHTML.length,sHTML) );
+  return;
+});
+
+app.listen(5678); //start the server
+
+console.log('Server is running...');
